e1d375280976f5658f367978383536e4a1354fa0
diana767 <152249430+diana767@users.noreply.github.com>
2024-11-02 13:10:35 +0500

    Add files via upload

---
+++ YP_rubtsova_zad2/YP_rubtsova_zad2/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace YP_rubtsova_zad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label10 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.AddNewProduct = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.AddProduct = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BuyPanel = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.AddCorz = new System.Windows.Forms.Button();
+            this.Buy = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.AddNewProduct.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.BuyPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(977, 711);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label10);
+            this.tabPage1.Controls.Add(this.listBox2);
+            this.tabPage1.Controls.Add(this.label9);
+            this.tabPage1.Controls.Add(this.label7);
+            this.tabPage1.Controls.Add(this.listBox3);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.AddNewProduct);
+            this.tabPage1.Controls.Add(this.BuyPanel);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(969, 682);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Shop";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(607, 445);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(199, 25);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Товары в корзине:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(489, 251);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(453, 132);
+            this.listBox2.TabIndex = 11;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(334, 22);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(275, 29);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Прибыль магазина - 0р";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(607, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(183, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "История продаж";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(524, 488);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(418, 180);
+            this.listBox3.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(166, 445);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Список товаров";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(40, 488);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(418, 180);
+            this.listBox1.TabIndex = 6;
+            // 
+            // AddNewProduct
+            // 
+            this.AddNewProduct.Controls.Add(this.label5);
+            this.AddNewProduct.Controls.Add(this.numericUpDown2);
+            this.AddNewProduct.Controls.Add(this.AddProduct);
+            this.AddNewProduct.Controls.Add(this.label4);
+            this.AddNewProduct.Controls.Add(this.label3);
+            this.AddNewProduct.Controls.Add(this.textBox2);
+            this.AddNewProduct.Controls.Add(this.label2);
+            this.AddNewProduct.Controls.Add(this.textBox1);
+            this.AddNewProduct.Location = new System.Drawing.Point(9, 76);
+            this.AddNewProduct.Margin = new System.Windows.Forms.Padding(4);
+            this.AddNewProduct.Name = "AddNewProduct";
+            this.AddNewProduct.Size = new System.Drawing.Size(295, 343);
+            this.AddNewProduct.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(40, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(196, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Добавление товара";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(32, 199);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(243, 22);
+            this.numericUpDown2.TabIndex = 9;
+            // 
+            // AddProduct
+            // 
+            this.AddProduct.Location = new System.Drawing.Point(63, 241);
+            this.AddProduct.Margin = new System.Windows.Forms.Padding(4);
+            this.AddProduct.Name = "AddProduct";
+            this.AddProduct.Size = new System.Drawing.Size(145, 66);
+            this.AddProduct.TabIndex = 8;
+            this.AddProduct.Text = "Добавить продукт";
+            this.AddProduct.UseVisualStyleBackColor = true;
+            this.AddProduct.Click += new System.EventHandler(this.AddProduct_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(28, 180);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Количество продукта";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Цена продукта";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(32, 142);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(241, 24);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(28, 70);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Название продукта";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 90);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 24);
+            this.textBox1.TabIndex = 0;
+            // 
+            // BuyPanel
+            // 
+            this.BuyPanel.Controls.Add(this.numericUpDown1);
+            this.BuyPanel.Controls.Add(this.label8);
+            this.BuyPanel.Controls.Add(this.AddCorz);
+            this.BuyPanel.Controls.Add(this.Buy);
+            this.BuyPanel.Controls.Add(this.label1);
+            this.BuyPanel.Controls.Add(this.textBox3);
+            this.BuyPanel.Location = new System.Drawing.Point(334, 76);
+            this.BuyPanel.Margin = new System.Windows.Forms.Padding(4);
+            this.BuyPanel.Name = "BuyPanel";
+            this.BuyPanel.Size = new System.Drawing.Size(608, 114);
+            this.BuyPanel.TabIndex = 5;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 77);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(243, 22);
+            this.numericUpDown1.TabIndex = 11;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 58);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(150, 16);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Количество продукта";
+            // 
+            // AddCorz
+            // 
+            this.AddCorz.Location = new System.Drawing.Point(255, 15);
+            this.AddCorz.Margin = new System.Windows.Forms.Padding(4);
+            this.AddCorz.Name = "AddCorz";
+            this.AddCorz.Size = new System.Drawing.Size(152, 53);
+            this.AddCorz.TabIndex = 5;
+            this.AddCorz.Text = "Добавить в корзину";
+            this.AddCorz.UseVisualStyleBackColor = true;
+            this.AddCorz.Click += new System.EventHandler(this.AddCorz_Click);
+            // 
+            // Buy
+            // 
+            this.Buy.Location = new System.Drawing.Point(431, 14);
+            this.Buy.Margin = new System.Windows.Forms.Padding(4);
+            this.Buy.Name = "Buy";
+            this.Buy.Size = new System.Drawing.Size(152, 54);
+            this.Buy.TabIndex = 4;
+            this.Buy.Text = "Купить";
+            this.Buy.UseVisualStyleBackColor = true;
+            this.Buy.Click += new System.EventHandler(this.Buy_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 10);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Название продукта";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(16, 30);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(205, 24);
+            this.textBox3.TabIndex = 2;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.tabPage2.Size = new System.Drawing.Size(969, 682);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Playlist";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 711);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.AddNewProduct.ResumeLayout(false);
+            this.AddNewProduct.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.BuyPanel.ResumeLayout(false);
+            this.BuyPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel AddNewProduct;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button AddProduct;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel BuyPanel;
+        private System.Windows.Forms.Button AddCorz;
+        private System.Windows.Forms.Button Buy;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ YP_rubtsova_zad2/YP_rubtsova_zad2/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace YP_rubtsova_zad2
+{
+    public partial class Form1 : Form
+    {
+        private Shop shop;
+        private Dictionary<Product, int> cart;
+        public Form1()
+        {
+            InitializeComponent();
+            shop = new Shop();
+            cart = new Dictionary<Product, int>();
+        }
+
+        private void UpdateProductList()
+        {
+            listBox1.Items.Clear();
+            foreach (var product in shop.ProductsDictionary())
+            {
+                listBox1.Items.Add(product.Key.GetInfo() + "; Количество: " + product.Value);
+            }
+        }
+
+        private void UpdateCartList()
+        {
+            listBox3.Items.Clear();
+            foreach (var item in cart)
+            {
+                listBox3.Items.Add(item.Key.GetInfo() + "; Количество: " + item.Value);
+            }
+        }
+
+        private void AddProduct_Click(object sender, EventArgs e)
+        {
+
+            string name = textBox1.Text;
+            decimal price;
+            int count = (int)numericUpDown2.Value;
+
+            if (decimal.TryParse(textBox2.Text, out price) && !string.IsNullOrWhiteSpace(name))
+            {
+                shop.CreateProduct(name, price, count);
+                UpdateProductList();
+                textBox1.Clear();
+                textBox2.Clear();
+                numericUpDown2.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("Введите корректные данные товара.");
+            }
+        }
+
+        private void Buy_Click(object sender, EventArgs e)
+        {
+            foreach (var item in cart)
+            {
+                listBox2.Items.Add($"Продано: {item.Key.GetInfo()}; Количество: {item.Value}");
+            }
+            cart.Clear();
+            UpdateCartList(); UpdateRevenueLabel();
+            MessageBox.Show("Покупка завершена!");
+        }
+        private void UpdateRevenueLabel()
+        {
+            label9.Text = $"ПРИЫБЫЛЬ МАГАЗИНА - {shop.GetRevenue()} руб.";
+        }
+
+        private void AddCorz_Click(object sender, EventArgs e)
+        {
+            string productName = textBox3.Text;
+            int count = (int)numericUpDown1.Value;
+
+            Product product = shop.FindByName(productName);
+            if (product != null && shop.ProductsDictionary()[product] >= count)
+            {
+                if (cart.ContainsKey(product))
+                {
+                    cart[product] += count;
+                }
+                else
+                {
+                    cart[product] = count;
+                }
+                UpdateCartList();
+                shop.Sell(product, count);
+                UpdateProductList();
+                textBox3.Clear();
+                numericUpDown1.Value = 0;
+            }
+            else
+            {
+                MessageBox.Show("Товар не найден или недостаточно на складе.");
+            }
+        }
+    }
+}
